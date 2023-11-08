5af38f2c7690334e5055d31550114b5e852f7b81
M0lt0 <bodymgahed@yahoo.com>
2023-11-08 19:39:47 -0300

    day 2 level 2

--- myProcess/day2/exc/exc-2.js
+++ myProcess/day2/exc/exc-2.js
@@ -72,6 +72,33 @@ console.log(bec.substring(0,31) ,bec.substring(54));
 
 
 
+//Exercises: Level 3
+
+
+let love = 'Love is the best thing in this world. Some found their love and some are still looking for their love.' ;
+let reg = /love/g;
+let regTow = /%$@*&#/g;
+let regthree = /D/ ;
+let mixSentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+let salarySentence =  'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
+let arr = salarySentence.split(" ");
+let  salary;
+function calc(arr, i){
+console.log(arr[i])
+for(let i = 0 ; i < arr.length; i++){
+    // if( i.valueOf() == i.parseInt){
+    //     salary += salary ;
+    // }
+    console.log(i.valueOf())
+}
+}
+console.log(love.search(reg)); // need t check
+console.log(bec.match('because'));
+console.log(mixSentence.replace(/%$@&#/g," ")); // need t check
+console.log();
+
+
+
 
 
 
