5d889921d3194390ab83239a225668660ea2520b
M0lt0 <bodymgahed@yahoo.com>
2023-11-09 13:41:27 -0300

    day 3 level 1

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,6 @@
 // this is your main.js script
+let number = prompt('Enter number', 'number goes here')
+console.log(number)
+
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree) // result will be true or false based on what you click on the dialog box
